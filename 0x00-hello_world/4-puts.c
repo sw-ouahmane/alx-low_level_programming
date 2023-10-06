648a5c37f938058302b634830cbdda3a5c77658e
abdallah <sw.ouahmane@gmail>
2023-10-05 17:02:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -A program that print a line wit puts fuction
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
