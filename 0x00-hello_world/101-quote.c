5567c48b9770921890f5c50f2107cfabbba9c4dc
abdallah <sw.ouahmane@gmail>
2023-10-06 17:03:20 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a line to the standard error.
+ *
+ * Return: 1 (Indicates an issue)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		    stderr);
+return (1);
+}
