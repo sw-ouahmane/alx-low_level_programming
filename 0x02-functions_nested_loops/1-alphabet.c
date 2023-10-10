c6967fde6c3512fac13c2b3eb002e1b1f61286c4
abdallah <sw.ouahmane@gmail>
2023-10-10 11:31:21 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main who is responsible for printing void print_alphabet(void).
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c= 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
