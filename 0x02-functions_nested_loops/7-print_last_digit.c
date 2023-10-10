07d5e388c0fe21eca5b5c054fe2926acf1da4198
abdallah <sw.ouahmane@gmail>
2023-10-10 16:58:14 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,13 @@
 int print_last_digit(int n)
 {
 int last_digit;
-
-    /* Ensure the number is positive or zero */
-if (n < 0)
-n = -n;
-
-    /* Extract the last digit by taking the remainder when divided by 10 */
 last_digit = n % 10;
 
-    /* Print the last digit using _putchar */
+    /* Ensure the number is positive or zero */
+if (last_digit < 0)
+{
+	last_digit = last_digit * -1;
+}
 _putchar(last_digit + '0');
-
 return (last_digit);
 }
