9396fa6284604e5cc57e07e7f56800dd47d41056
abdallah <sw.ouahmane@gmail>
2023-10-19 04:44:30 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 int dest_len = 0;
 
-// Find the length of dest
+
 while (dest[dest_len] != '\0')
 {
 dest_len++;
 }
 
-// Append at most 'n' characters from src to dest
+
 for (int i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[dest_len] = src[i];
