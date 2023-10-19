dd8e684f8d827d1041e3039ff7a0f07a2779d6ad
abdallah <sw.ouahmane@gmail>
2023-10-19 16:07:55 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
+/**
+* print_number - Modify the value pointed to by p to set b[2] to 98.
+* @n: input integer.
+* Return: no return.
+*/
+
 int main(void)
 {
 int n;
-int a[5];
+int b[5];
 int *p;
 
-a[2] = 1024;
+b[2] = 1024;
 p = &n;
 
-/* Modify the value pointed to by p to set a[2] to 98 */
+
 *(p + 5) = 98;
 
-/* This will print "a[2] = 98" */
-printf("a[2] = %d\n", a[2]);
+
+printf("b[2] = %d\n", b[2]);
 return (0);
 }
 
