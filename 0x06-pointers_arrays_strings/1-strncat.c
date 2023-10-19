9f3d0a411359b04e6026187165b9d2bf14568524
abdallah <sw.ouahmane@gmail>
2023-10-19 04:35:39 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strncat - concatenates two strings,
+* @dest: destination.
+* @src: source.
+* @n: amount of bytes used from src.
+* Return: the pointer to dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = 0;
+
+// Find the length of dest
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+
+// Append at most 'n' characters from src to dest
+for (int i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[dest_len] = src[i];
+dest_len++;
+}
+
+return dest;
+}
