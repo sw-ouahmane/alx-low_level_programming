e9f4c4c201e2f76a95bb61dcf877a0340274e191
abdallah <sw.ouahmane@gmail>
2023-10-23 15:26:56 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_chessboard - prints chessboard.
- * @a: matrix
-*/
-
-void print_chessboard(char (*a)[8])
-{
+void print_chessboard(char (*a)[8]) {
 int i, j;
 
-for (i = 0; i < 8; i++)
-{
-for (j = 0; j < 8; j++)
-{
-_putchar(*(*(i + a) + j));
+for (i = 0; i < 8; i++) {
+for (j = 0; j < 8; j++) {
+putchar(a[i][j]);
+putchar(' '); // Add space for visual separation
 }
-_putchar('\n');
+putchar('\n'); // Move to the next line after each row
 }
+}
+
+int main() {
+char chessboard[8][8] = {
+{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+};
+
+print_chessboard(chessboard);
+
+return 0;
 }
 
