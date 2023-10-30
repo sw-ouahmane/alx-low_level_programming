510d4eb6efa17499201e952fd01b3551c2a43af1
abdallah <sw.ouahmane@gmail>
2023-10-30 06:35:49 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -27,7 +27,7 @@ else if (cents >= 10)
 cents -= 10;
 else if (cents >= 5)
 cents -= 5;
-else if (cents >= 2
+else if (cents >= 2)
 cents -= 2;
 else if (cents >= 1)
 cents -= 1;
@@ -36,3 +36,4 @@ ncoins += 1;
 printf("%d\n", ncoins);
 return (0);
 }
+
