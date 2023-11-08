ca74c1271e9ca641b31f1fc8d6f9dcf0fcfdef95
abdallah <sw.ouahmane@gmail>
2023-11-08 15:32:23 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+
+if (!array || !action)
+return;
+
+for (i = 0; i < size; i++)
+action(array[i]);
+}
+
