46e967dafc23b6723f74bf6ff4cf7f7395884411
abdallah <sw.ouahmane@gmail>
2024-01-15 16:42:17 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * add - add two integers
+ * @a:first int
+ * @b:second int
+ * Return: addition
+ */
+int div(int a ,int b)
+{
+	return(a/b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * add - add two integers
+ * @a:first int
+ * @b:second int
+ * Return: addition
+ */
+int mod(int a ,int b)
+{
+	return(a%b);
+}
